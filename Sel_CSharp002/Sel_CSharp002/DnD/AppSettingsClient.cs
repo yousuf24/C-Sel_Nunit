1bf778bd27522ff6e0da43d5ef77138b2afe6713
aqieb.javed1996 <aqieb.javed1996@gmail.com>
2023-06-22 21:20:44 +0530

    added changes on Jun22-2023

--- Sel_CSharp002/Sel_CSharp002/DnD/AppSettingsClient.cs
+++ Sel_CSharp002/Sel_CSharp002/DnD/AppSettingsClient.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sel_CSharp002.DnD;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +28,7 @@ namespace Sel_CSharp002.DnD
     }
     class AppSettingsClient : IAppSettingsClient, ITryAppSettingsClient, ITypeAppSettingsClient
     {
-        private readonly IAppSettingsClient _restClient;
+        private readonly IAppSettingsRestClient _restClient;
         private DefaultContext defaultContext;
 
         private IParameterValidator Validator { get; set; } = new ParameterValidator();
@@ -32,10 +37,18 @@ namespace Sel_CSharp002.DnD
         {
 
         }
+        public AppSettingsClient(IConfiguration config):this(new DefaultContext(config))
+        {
+
+        }
         public AppSettingsClient(IClientContext context)
         {
-            //_restClient = new AppSettingRestClient(context);
-            //TODO:
+            _restClient = new AppSettingsRestClient(context);
+            
+        }
+        public AppSettingsClient(IAppSettingsRestClient restClient)
+        {
+            _restClient = restClient;
         }
 
         public AppSettingsClient(DefaultContext defaultContext)
@@ -139,3 +152,138 @@ public string EnvironmentalPrefix { get ; set ; }
 LOG
 ----------
 #endif
+public class AppSettingsRestClient : IAppSettingsRestClient
+{
+    private readonly IClientContext clientcontext;
+    public HttpClient HttpClient { get; }
+    public AppSettingsRestClient(IClientContext clientcontext):this(clientcontext,new HttpClient()) { }
+    public AppSettingsRestClient(IClientContext clientcontext,HttpClient client)
+    {
+        this.clientcontext = clientcontext;
+        HttpClient = client;
+        SetSecurityHeader();
+    }
+    public void HandleEx(AppSettingsClientException ex)
+    {
+        if(ex!=null && ex.InnerException?.Message.Contains("No such host is known")==true)
+        {
+            throw new AppSettingsClientException("ConfigService url does not exist or misconfigured:" + clientcontext.AppSettingsServiceUrl);
+        }
+        if (ex != null && ex.InnerException?.Message.Contains("401") == true)
+        {
+            throw new AppSettingsClientException("401 " + ex.Message);
+        }
+        if (ex != null && ex.InnerException?.Message.Contains("400") == true)
+        {
+            throw new AppSettingsClientException("400 " + ex.Message);
+        }
+        throw new AppSettingsClientException("500 " + ex.Message, ex);
+    }
+    public Task<string> GetAsync(string section, string key)
+    {
+        Task<string> task = null;
+        return Task<string>.Factory.StartNew(delegate
+        {
+            string empty = string.Empty;
+            string requestUrl = clientcontext.AppSettingsServiceUrl + "/secrets?applicationName=" + clientcontext.AppName + "&sectionName=" + section+"&keyName="+key;
+            return ReadResponse(requestUrl);
+        });
+    }
+
+    public Task<string> GetSecretAsync(string secretName, string section = null)
+    {
+        return Task<string>.Factory.StartNew(delegate
+        {
+            string empty = string.Empty;
+            string requestUrl = clientcontext.AppSettingsServiceUrl + "/secrets?applicationName=" + clientcontext.AppName + "&secretName=" + secretName + "&sectionName=" + section;
+            return ReadResponse(requestUrl);
+        });
+    }
+
+    public Task<T> GetSectionAsync<T>(string section)
+    {
+        return Task<T>.Factory.StartNew(delegate
+        {
+            object obj = null;
+            string requestUrl = clientcontext.AppSettingsServiceUrl + "/secrets?applicationName=" + clientcontext.AppName +  "&sectionName=" + section;
+            string value= ReadResponse(requestUrl);
+            obj = JsonConvert.DeserializeObject<T>(value);
+            return (dynamic)obj;
+        });
+    }
+    private string ReadResponse(string requestUrl)
+    {
+        string text = string.Empty;
+        JObject jObject = null;
+        try
+        {
+            HttpResponseMessage result = HttpClient.GetAsync(requestUrl).Result;
+            text = result.Content.ReadAsStringAsync().Result;
+            result.EnsureSuccessStatusCode();
+            jObject = JsonConvert.DeserializeObject<JObject>(text);
+
+        }
+        catch (HttpRequestException innerException)
+        {
+            HandleEx(new AppSettingsClientException(text, innerException));
+            throw;
+        }
+        return jObject["result"].ToString();
+    }
+    private void SetSecurityHeader()
+    {
+        if (!string.IsNullOrEmpty(clientcontext.AppSettingsServiceSubscriptionkey))
+        {
+            HttpClient.DefaultRequestHeaders.Add("Ocp-Apim-subscription-Key", clientcontext.AppSettingsServiceSubscriptionkey);
+            HttpClient.DefaultRequestHeaders.Add("CT-API-Key", clientcontext.AppSettingsServiceSubscriptionkey);
+        }
+    }
+}
+public interface IAppSettingsRestClient
+{
+    Task<String> GetAsync(string section, string key);
+    Task<T> GetSectionAsync<T>(string section);
+    Task<String> GetSecretAsync(string secretName, string section = null);
+}
+[Serializable]
+public class AppSettingsClientException: CTException
+{
+    public AppSettingsClientException()
+    {
+
+    }
+    public AppSettingsClientException(string message): base(message)
+    {
+
+    }
+    public AppSettingsClientException(string message,Exception innerException) : base(message,innerException)
+    {
+
+    }
+    protected AppSettingsClientException(SerializationInfo serializationInfo,StreamingContext streamingContext)
+    {
+
+    }
+}
+[Serializable]
+public class CTException : Exception
+{
+    public CTException()
+    {
+
+    }
+    public CTException(string message)
+    {
+
+    }
+    public CTException(string message,Exception innerException)
+    {
+
+    }
+    public CTException(SerializationInfo serializationInfo, StreamingContext streamingContext):base(serializationInfo,streamingContext)
+    {
+
+    }
+
+
+}
--- Sel_CSharp002/Sel_CSharp002/TestData/AppSettingsClientConfig.cs
+++ Sel_CSharp002/Sel_CSharp002/TestData/AppSettingsClientConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Sel_CSharp002.DnD;
+using Sel_CSharp002.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,26 @@ namespace Sel_CSharp002.TestData
 {
     class AppSettingsClientConfig
     {
-        //static AppSettingsClient instance = null;
-        //public static string AppName { get; set; }
-        //public static AppSettingsClient
+        static AppSettingsClient instance = null;
+        public static string AppName { get; set; }
+        public static AppSettingsClient GetClient
+        {
+            get
+            {
+                if (instance == null)
+                    instance = Create();
+                return instance;
+            }
+        }
+        private static AppSettingsClient Create()
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile(ProjectConstants.projectAppSettingsFileName).Build();
+            var applicationSettings=new AppSettingsClient(config);
+
+            if (config.GetSection("appSettings").Exists())
+            {
+                AppName = config["appSettings:appName"];
+            }return applicationSettings;
+        }
     }
 }
