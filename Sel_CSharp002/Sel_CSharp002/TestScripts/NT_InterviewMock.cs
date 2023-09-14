14720993a4db7cbd10c00b68acfafcd2fa6a233c
yousuf24 <aqieb.javed1996@gmail.com>
2023-09-15 01:06:15 +0530

    09152023

---
+++ Sel_CSharp002/Sel_CSharp002/TestScripts/NT_InterviewMock.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Sel_CSharp002.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sel_CSharp002.TestScripts
+{
+    [TestFixture]
+    [Parallelizable]
+    class NT_InterviewMock:BaseSetup
+    {
+        [Test]
+        public void ExecuteB2B()
+        {
+            By byLF = By.XPath("(//a[text()=\"Ladies' Fingers (Loose)\"]/ancestor::div[@qa='product_name']/following-sibling::div//button[text()='Add '])[last()]");
+            _seleniumHelper.OpenUrl("https://www.bigbasket.com/");
+            _seleniumHelper.WaitForPageToLoad();
+            _seleniumHelper.WaitTillExists(byLF);
+            Console.WriteLine("Ladies finger is found");
+
+        }
+
+    }
+}
